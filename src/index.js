729c8f6890fe2b08e14a8113fd3671f341ddd001
Daniela Cordoba <dacorac43@gmail.com>
2024-01-14 17:16:25 +1000

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -2,40 +2,35 @@ import { takepicture, clearphoto } from './webcam.js';
 
 let width = 320;
 let height = 0;
-
 let streaming = false;
-
 let video = null;
 let canvas = null;
 let photo = null;
 let startbutton = null;
 
-(function() {
+function initialize() {
   video = document.getElementById('video');
   canvas = document.getElementById('canvas');
   photo = document.getElementById('photo');
   startbutton = document.getElementById('startbutton');
 
   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-  .then(function(stream) {
-    video.srcObject = stream;
-    video.play();
-  })
-  .catch(function(err) {
-    console.log("An error occurred: " + err);
-  });
+    .then(function (stream) {
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch(function (err) {
+      console.log("An error occurred: " + err);
+    });
 
-  video.addEventListener('canplay', function(ev){
+  video.addEventListener('canplay', function (ev) {
     if (!streaming) {
-      height = video.videoHeight / (video.videoWidth/width);
-    
-      // Firefox currently has a bug where the height can't be read from
-      // the video, so we will make assumptions if this happens.
-    
+      height = video.videoHeight / (video.videoWidth / width);
+
       if (isNaN(height)) {
-        height = width / (4/3);
+        height = width / (4 / 3);
       }
-    
+
       video.setAttribute('width', width);
       video.setAttribute('height', height);
       canvas.setAttribute('width', width);
@@ -44,28 +39,34 @@ let startbutton = null;
     }
   }, false);
 
-  startbutton.addEventListener('click', async function(ev){
-    let imageData = await takepicture(video, width, height);
+  startbutton.addEventListener('click', startStreaming, false);
 
-    const formData = new FormData();
-    formData.append('image', imageData, 'image.png');
-    
-    try {
-      const response = await fetch('/upload', {
+  clearphoto();
+}
+
+function startStreaming() {
+  takepicture(video, width, height)
+    .then(function (imageData) {
+      const formData = new FormData();
+      formData.append('image', imageData, 'image.png');
+
+      fetch('/upload', {
         method: 'POST',
         body: formData
-      });
-      const data = await response.json();
-      console.log('Image uploaded:', data);
-    }
-    catch(error)
-    {
-      console.error('Error uploading image:', error);
-    }
-    ev.preventDefault();
-  }, false);
-  
-  clearphoto();
-})();
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log('Image uploaded:', data);
+        })
+        .catch(function (error) {
+          console.error('Error uploading image:', error);
+        });
+    })
+    .catch(function (error) {
+      console.error('Error taking picture:', error);
+    });
+}
 
-
+initialize();
