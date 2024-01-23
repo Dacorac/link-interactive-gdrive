ba6c653b0f5b1e24fa9509731ca8972701e7e812
Daniela Cordoba <dacorac43@gmail.com>
2024-01-23 21:18:43 +1000

    Updating and cleaning code

--- static/webcam.py
+++ static/webcam.py
@@ -1,98 +1,72 @@
 import os
-import datetime
 import subprocess
 import requests
-import base64
-import io
 import re 
 
-# def create_blob_from_base64(image_base64):
-#     try:
-#         # Add padding if necessary
-#         padded_base64 = image_base64 + '=' * (-len(image_base64) % 4)
-        
-#         # Decode the base64 string
-#         image_bytes = base64.b64decode(padded_base64)
-        
-#         # Convert the bytes to a blob
-#         with io.BytesIO(image_bytes) as image_bytes_io:
-#             blob = image_bytes_io.getvalue()
-#             return blob
-#         # image_bytes = base64.b64decode(image_base64)
-#         # with io.BytesIO(image_bytes) as image_bytes_io:
-#         #     blob = image_bytes_io.getvalue()
-#         #     return blob
-
-#     except Exception as e:
-#         raise RuntimeError("Failed to create Blob from base64 image:", str(e)) from e
-
 def capture_image():
-    # call the .sh to capture the image
-    try:
-        '/home/pi/link-interactive-webcam/webcam.sh' 
-        output = subprocess.check_output(['/home/pi/link-interactive-webcam/webcam.sh'], stderr=subprocess.STDOUT, text=True)
-        # output = subprocess.check_output(['/home/juliangonzalez/Documentos/link-interactice-gdrive-py/webcam.sh'], stderr=subprocess.STDOUT, text=True) #JULI
+  # call the .sh to capture the image
+  try:
+    '/home/pi/link-interactive-webcam/webcam.sh' 
+    output = subprocess.check_output(['/home/pi/link-interactive-webcam/webcam.sh'], stderr=subprocess.STDOUT, text=True)
 
-        match = re.search(r"'([^']*)'", output)
+    match = re.search(r"'([^']*)'", output)
 
-        if match:
-            image_data = match.group(1)
-            print(image_data)  
-        else:
-            print("The desired string was not found in the provided shell command.")
+    if match:
+      image_data = match.group(1)
+      print(image_data)  
+    else:
+      print("The desired string was not found in the provided shell command.")
 
-        # Extract the path
-        rel_path = image_data.split('/')[-1]
+    # Extract the path
+    rel_path = image_data.split('/')[-1]
 
-        # Print the path
-        print(rel_path)
+    # Print the path
+    print(rel_path)
 
-        print('Image data')
-        print(image_data)
-
-        return image_data, rel_path
-    except Exception as e:
-        raise RuntimeError(str(e)) from e
+    return image_data, rel_path
+  except Exception as e:
+      raise RuntimeError(str(e)) from e
 
 async def upload_image(image_data):
-    try:
-        # Assuming `image_data` contains the file path or binary data of the image
-        files = {'image': open(image_data, 'rb')}  # Use 'image' instead of 'file'
+  try:
+    # Assuming `image_data` contains the file path or binary data of the image
+    files = {'image': open(image_data, 'rb')}  # Use 'image' instead of 'file'
 
-        response = requests.post('http://localhost:3000/upload', files=files)
+    response = requests.post('http://localhost:3000/upload', files=files)
 
-        # Verificar el resultado de la solicitud
-        if response.status_code == 200:
-            print('El archivo se ha enviado exitosamente.')
-        else:
-            print('Hubo un problema al enviar el archivo:', response.status_code)
+    # Check the reponse status
+    if response.status_code == 200:
+      print('The file has been uploaded successfully.')
+      os.remove(image_data) # Remove local image when successful
+    else:
+      print('There is an unexpected error while uploading the file: ', response.status_code)
 
+<<<<<<< Updated upstream
         data = response.json()
         
         print('Image uploaded:', data)
         os.remove(image_data)
         return data
+=======
+    data = response.json()
+    print('Image uploaded:', data)
+    return data
+>>>>>>> Stashed changes
 
-    except requests.exceptions.RequestException as error:
-        print('Error uploading image:', error)
-        raise
+  except requests.exceptions.RequestException as error:
+    print('Error uploading image:', error)
+    raise
 
-    except Exception as error:
-        print('Error taking picture:', error)
-        raise
+  except Exception as error:
+    print('Error taking picture:', error)
+    raise
 
 async def main():
-    # capture image
-    image_data, rel_path = capture_image()
+  # capture image
+  image_data, rel_path = capture_image()
 
-    # convert image to blob
-    # blob = create_blob_from_base64(image_data)
-    # upload image
-    await upload_image(image_data)
-
-    # print the response
-    print()
+  await upload_image(image_data)
 
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+  import asyncio
+  asyncio.run(main())
--- static/webcam.sh
+++ static/webcam.sh
@@ -3,17 +3,7 @@
 DATE=$(date +"%Y-%m-%d_%H%M")
 
 # capture the image
-image_data=$(fswebcam -r 1280x720 --no-banner -q /home/pi/$DATE.jpg)
+image_data=$(fswebcam -r 1280x720 --no-banner /home/pi/$DATE.jpg)
 
 # return the image data
 echo $image_data
-
-
-#ESTO ES PARA LA EJECUCION DE JULI
-# DATE=$(date +"%Y-%m-%d_%H%M")
-
-# # capture the image using cheese and save it to a file
-# fswebcam /home/juliangonzalez/Documentos/$DATE.jpg
-
-# # return the image file path
-# echo "/home/juliangonzalez/Documentos/$DATE.jpg"
