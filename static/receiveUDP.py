baa6a8633c8774f8723d2053e37e6e2d5cfa4baa
Julian Gonzalez <julian.gonzpi92@gmail.com>
2024-01-28 22:25:20 -0500

    try except and retry udp connection

--- static/receiveUDP.py
+++ static/receiveUDP.py
@@ -1,46 +1,67 @@
 import socket
 import subprocess
+import time
 
 buffersize = 1024
 
 serverIP = '10.1.1.3'
 severport = 5000
+retry_number = 0
+
 
 def execute_python_command(command):
   # Run command in shell
   subprocess.call(command, shell=True)
 
 def receive_udp_message_and_execute_python_command():
-  # Create UDP socket
-  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  try:
+    # Create UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-  # Bind to port 5000
-  sock.bind((serverIP, severport))
+    # Bind to port 5000
+    sock.bind((serverIP, severport))
 
-  print('Listening...')
+    print('Listening...')
 
-  while True:
-    # Waiting for message
-    data, addr = sock.recvfrom(buffersize)
+    while True:
+      # Waiting for message
+      data, addr = sock.recvfrom(buffersize)
 
-    # Decode message 
-    command = data.decode('utf-8')
+      # Decode message 
+      command = data.decode('utf-8')
 
-    print(command)
-    print(addr[0])
+      print(command)
+      print(addr[0])
+      
+      if command == 'hello':
+        messageFromServer = 'Hello from server'
+      elif command == 'take photo':
+        execute_python_command('python3 /home/pi/link-interactive-webcam/webcam.py')
+        messageFromServer = 'Webcam script executed.'
+      elif command == 'close':
+        messageFromServer = 'Bye from server'
+        break
+
+      # Send response to client
+      bytestosend = messageFromServer.encode('utf-8')
+      sock.sendto(bytestosend, addr)
+
+    succesfull_message = "Script exceuted succesfully"
+    success_to_send = succesfull_message.encode('utf-8')
+
+    sock.sendto(success_to_send, addr)
+  except Exception as ex:
+    if retry_number < 5:
+      retry_number = retry_number + 1
+      time.sleep(10)
+      receive_udp_message_and_execute_python_command()
+    else :
+      error_message = f"An error has ocurred while processing command: {str(ex)}"
+      error_to_send = error_message.encode('utf-8')
+      retry_number = 0
+      sock.sendto(error_to_send, addr)
     
-    if command == 'hello':
-      messageFromServer = 'Hello from server'
-    elif command == 'take photo':
-      execute_python_command('python3 /home/pi/link-interactive-webcam/webcam.py')
-      messageFromServer = 'Webcam script executed.'
-    elif command == 'close':
-      messageFromServer = 'Bye from server'
-      break
-
-    # Send response to client
-    bytestosend = messageFromServer.encode('utf-8')
-    sock.sendto(bytestosend, addr)
+    
 
 if __name__ == "__main__":
   receive_udp_message_and_execute_python_command()
